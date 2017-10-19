bd0d40dc440d420d8cdc6dd9b90163d5203d8c7d
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-19 18:16:21 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,9 @@
 class Hash
   def keys_of(*arguments)
-    
+    matches = []
+    self.each do |key, value|
+      matches.push(key if arguments.include?(value))
+    end
+    return matches
   end
 end
